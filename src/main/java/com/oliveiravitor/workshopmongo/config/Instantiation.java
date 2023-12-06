f784b69b6c44727e41e26f258fcdb15d8ec9b04f
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-12-06 15:36:57 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/oliveiravitor/workshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.oliveiravitor.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.oliveiravitor.workshopmongo.domain.User;
+import com.oliveiravitor.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.deleteAll();
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
