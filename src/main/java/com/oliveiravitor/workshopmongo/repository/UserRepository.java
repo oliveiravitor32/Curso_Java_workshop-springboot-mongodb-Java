8777ef0b6aa62a0e881f4029df0daf91cf941b2d
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-12-06 15:32:44 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/oliveiravitor/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.oliveiravitor.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oliveiravitor.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
